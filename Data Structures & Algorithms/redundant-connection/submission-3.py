4600921b094a02c174bdd8c154cd49fb09f1984e
Anurag Ray Chowdhury <110551628+anuragraychowdhury@users.noreply.github.com>
2026-05-01 19:29:54 -0400

    Add: redundant-connection - submission-3

---
+++ Data Structures & Algorithms/redundant-connection/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        parent = [i for i in range(n + 1)]
+        rank = [1] * (n+1)
+
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(a,b):
+            parent_a = find(a)
+            parent_b = find(b)
+
+            if rank[parent_a] >= rank[parent_b]:
+                parent[parent_b] = parent_a
+                rank[parent_a] += rank[parent_b]
+            else:
+                parent[parent_a] = parent[parent_b]
+                rank[parent_b] += rank[parent_a]
+            
+            return
+        
+        for a,b in edges:
+            if find(a) == find(b):
+                return [a,b]
+            else:
+                union(a,b)
+
+
