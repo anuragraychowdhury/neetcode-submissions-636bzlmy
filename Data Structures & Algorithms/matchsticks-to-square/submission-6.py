60fe0357074e8d9707855d93b6ddb21136999978
Anurag Ray Chowdhury <110551628+anuragraychowdhury@users.noreply.github.com>
2026-06-15 17:11:22 -0400

    Add: matchsticks-to-square - submission-6

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-6.py
@@ -0,0 +1,35 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        matchsticks.sort(reverse=True)
+        if sum(matchsticks) % 4 != 0:
+            return False
+        one_side = sum(matchsticks) // 4
+        
+        buckets = [0] * 4
+        def find_side(index, buckets):
+            if index == len(matchsticks):
+                for i in range(len(buckets)):
+                    if buckets[i] != one_side:
+                        return False
+                return True
+            
+            for j in range(len(buckets)):
+                if buckets[j] + matchsticks[index] > one_side:
+                    continue
+                else:
+                    buckets[j] += matchsticks[index]
+                    side_check = find_side(index + 1, buckets)
+                    if side_check == True:
+                        return True
+                    else:
+                        buckets[j] -= matchsticks[index]
+                        if buckets[j] == 0:
+                            break
+            return False
+        res = find_side(0, buckets)
+        return res
+
+        
+
+
+
