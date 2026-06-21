33b0f8f1b40c41c9e093a63dac2bc7109cb358f0
Anurag Ray Chowdhury <110551628+anuragraychowdhury@users.noreply.github.com>
2026-06-20 20:44:52 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map = {}
+        map2 = {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            print(s[i])
+            map[s[i]] = map.get(s[i],0) + 1
+            map2[t[i]] = map2.get(t[i],0) + 1
+        
+        print(sorted(map.items()))
+        print(sorted(map2.items()))
+
+        # print(map.values())
+        # print(map2.values())
+
+        return sorted(map.items()) == sorted(map2.items())
+
+
+
